dcdfa0a19fc9a49228e3ef2210fdc81beb24ddf8
Zuzana Svorenova <zuzana.svorenova@gmail.com>
2020-11-10 11:58:50 -0600

    data exploration

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
 // @TODO: YOUR CODE HERE!
+
+d3.csv("../assets/data/data.csv").then(function(censusData, err) {
+    if (err) throw err;
+
+    // Data Exploration
+    console.log(censusData)
+     
+    
+
+
+
+}).catch(function(error) {
+    console.log(error);
+});
+    
